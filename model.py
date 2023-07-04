703d2f0ddd2ea1b18123cfeb3c39c3c20f718a54
Anas <anaselnahef12@gmail.com>
2023-07-05 00:33:45 +0300

    changes

--- model.py
+++ model.py
@@ -1,6 +1,10 @@
 import torch
 import torch.nn as nn
 
+#If the hidden size is too small, the network may not have enough capacity to represent the underlying patterns in the data, leading to underfitting. Underfitting means the model cannot capture the complexity of the problem, resulting in poor performance.
+
+
+#if the hidden size is too large, the network may become overly complex and start to memorize the training examples instead of learning generalizable patterns from them. This can lead to overfitting, where the model performs well on the training data but fails to generalize to new, unseen data.
 
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
@@ -25,7 +29,7 @@ This code defines a class called NeuralNet, which represents a neural network mo
 The NeuralNet class has the following features:
 
 It has an __init__ method that initializes the model and defines its layers. The __init__ method takes in three arguments: input_size, hidden_size, and num_classes, which represent the size of the input layer, the size of the hidden layers, and the number of output classes, respectively. It uses these arguments to instantiate three linear layers (l1, l2, l3) using PyTorch's nn.Linear class and a ReLU activation layer (relu) using PyTorch's nn.ReLU class.
-It has a forward method that defines the forward pass of the model. The forward method takes in an input tensor x and applies the linear layers and the ReLU activation function to it in sequence, returning the final output of the model. It's important to note that the forward method does not apply an activation function or a softmax function at the end, which means that the output of the model is not normalized and does not represent a probability distribution.
+It has a forward method that defines the forward pass of the model. The forward method takes in an input ```tensor``` x and applies the linear layers and the ReLU activation function to it in sequence, returning the final output of the model. It's important to note that the forward method does not apply an activation function or a softmax function at the end, which means that the output of the model is not normalized and does not represent a probability distribution.
 
 1.input_size represents the size of the input layer of the model. It specifies the number of input features that the model expects. For example, if the model is a classifier that takes in a sentence as input and the sentence is represented as a vector of word embeddings, then input_size would be the length of the word embedding vector.
 
@@ -47,4 +51,14 @@ l3: This is a linear layer that takes in the output of the second linear layer (
 
 
 Overall, these layers form a feedforward neural network with three linear layers and ReLU activation. The input is passed through the layers in sequence, and the final output is produced by the third linear layer (l3).
+
+
+
+The linear layer represents a fully connected layer, where each neuron in the input is connected to each neuron in the output.
+
+-linear layer means that each neuron in the input is connected to each neuron in the output. This is also known as a fully connected layer.
+
+-It allows information to flow from each neuron in the input layer to every neuron in the output layer through a set of weights and biases. The weights and biases are parameters of the model that are learned during training. They are used to transform the input into the output. The weights and biases are initialized randomly at the start of training and are updated during training using an optimization algorithm such as gradient descent. The goal of training is to find the optimal values for the weights and biases that minimize the loss function. The loss function is a measure of how well the model performs on the training data. It is used to evaluate the model's performance and to update the weights and biases during training. The loss function is typically a function of the model's predictions and the actual labels of the training data. For example, if the model is a classifier that predicts the sentiment of a sentence (positive, negative, neutral), then the loss function would be a function of the model's predictions and the actual labels of the training data.
+
+-It is important to note that the linear layer does not apply an activation function to the output. This means that the output of the linear layer is not normalized and does not represent a probability distribution. The output of the linear layer is passed through a ReLU activation function, which normalizes the output and produces a probability distribution over the output classes. The ReLU activation function is defined as follows: ReLU(x) = max(0, x). It takes in a real number x and returns the maximum of 0 and x. This means that if x is negative, then ReLU(x) = 0, and if x is positive, then ReLU(x) = x. The ReLU activation function is used to introduce non-linearity into the model. This is important because linear models are only able to learn linear patterns in the data. Non-linear models are able to learn more complex patterns in the data. 
 """
--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import numpy as np
-import random
+
 import json
 
 import torch
@@ -22,22 +22,27 @@ for intent in intents['intents']:
     tags.append(tag)
     for pattern in intent['patterns']:
         # tokenize each word in the sentence
-        w = tokenize(pattern)
-        # add to our words list
+        w = tokenize(pattern) 
+        # add to our words list (('how', 'are'), ('', 'you'))
         all_words.extend(w)
-        # add to xy pair
-        xy.append((w, tag))
+        # add to xy pair x -> pattern, y -> tag 
+        xy.append((w, tag))  #[(["hello"],"grettings"),(["hey"],"grettings")]
 
+# print('all words', all_words,'\n')
 # stem and lower each word
 ignore_words = ['?', '.', '!']
-all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = [stem(w) for w in all_words if w not in ignore_words] #stem the words and remove the ignore words
 # remove duplicates and sort
-all_words = sorted(set(all_words))
+all_words = sorted(set(all_words)) 
 tags = sorted(set(tags))
 
-print(len(xy), "patterns")
-print(len(tags), "tags:", tags)
-print(len(all_words), "unique stemmed words:", all_words)
+# print(len(xy), "patterns")
+# print(len(tags), "tags:", tags)
+# print(len(all_words), "unique stemmed words:", all_words)
+
+# print(xy, "patterns\n")
+# print(tags, "tags: \n", )
+# print(all_words, "unique stemmed words:\n")
 
 # create training data
 X_train = []
@@ -46,19 +51,22 @@ for (pattern_sentence, tag) in xy:
     # X: bag of words for each pattern_sentence
     bag = bag_of_words(pattern_sentence, all_words)
     X_train.append(bag)
+    # print('bag of words: ',bag,'\n')
     # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
     label = tags.index(tag)
     y_train.append(label)
+    # print('label: ',label,'\n')
 
-X_train = np.array(X_train)
-y_train = np.array(y_train)
+X_train = np.array(X_train) #[[0,0,0,1,0,],[0,0,0,1,0,],[0,0,0,1,0,]]
+y_train = np.array(y_train) #[0,0,0,1,1,1]
+
+# print('data after preparation example: ',X_train,'\n')
+# print('labels example: ',y_train,'\n\n\n\n')
 
 # Hyper-parameters 
-num_epochs = 1000
-batch_size = 8
-learning_rate = 0.001
-input_size = len(X_train[0])
-hidden_size = 8
+batch_size = 8 #means that in every iteration, we train on 8 samples at the same time
+input_size = len(X_train[0]) #X_train[0] same as -> all_words
+hidden_size = 5
 output_size = len(tags)
 print(input_size, output_size)
 
@@ -82,27 +90,38 @@ train_loader = DataLoader(dataset=dataset,
                           batch_size=batch_size,
                           shuffle=True,
                           num_workers=0)
+#shuffle to prevent the model from learning the order of the data because it will affect the model accuracy
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
-
 # Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+criterion = nn.CrossEntropyLoss() # measures the discrepancy(تناقض) between the predicted probabilities and the true labels.
+# The CrossEntropyLoss function combines the softmax function and the negative log-likelihood loss
+# The negative log-likelihood loss then compares the predicted probabilities with the true labels and calculates the loss value
 
+learning_rate = 0.001
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) #optimze the model and update the parameters based on the lossx
+
+#The optimizer is responsible for updating the weights and biases of the model based on the computed gradients during backpropagation
+
+#overall goal is to iteratively update the parameters( weights and biases ) to minimize the loss function
+
+num_epochs = 1000
 # Train the model
 for epoch in range(num_epochs):
     for (words, labels) in train_loader:
         words = words.to(device)
         labels = labels.to(dtype=torch.long).to(device)
-        
+        # print('labels',labels,'\n')
+        # print('words',words,'\n')
         # Forward pass
         outputs = model(words)
+        # print('outputs',outputs,'\n')
         # if y would be one-hot, we must apply
         # labels = torch.max(labels, 1)[1]
         loss = criterion(outputs, labels)
-        
+        # print('loss',loss,'\n')
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
@@ -123,6 +142,7 @@ data = {
 "tags": tags
 }
 
+# print(data)
 FILE = "data.pth"
 torch.save(data, FILE)
 
